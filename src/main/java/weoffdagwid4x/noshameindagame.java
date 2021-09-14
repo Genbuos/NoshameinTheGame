c6b1db539713a174a9e7e1865f67c9f93dc1bc89
Genbuos <swiftypython18@gmail.com>
2021-09-14 19:53:13 -0400

    homework

---
+++ src/main/java/weoffdagwid4x/noshameindagame.java
@@ -0,0 +1,26 @@
+package weoffdagwid4x;
+
+import java.util.OptionalDouble;
+
+public class noshameindagame {
+    public static double discountedprice (double price, double discount) {
+
+        double p = 0;
+        double d = 0;
+        double discountammount = price * discount;
+
+        double total = price - discountammount;
+
+
+
+
+
+        return total;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(discountedprice(500, 0.10));
+
+
+    }
+}
